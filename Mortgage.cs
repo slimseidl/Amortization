cd304be83ff4af8e43f0cd3bac9cac4b2445f9dc
John Seidl <jseidl2@wgu.edu>
2025-07-28 14:11:26 -0500

    Basic amortization and payment breakdown

---
+++ Mortgage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amortization
+{
+    internal class Mortgage
+    {
+        public double MonthlyPayment(int principal, double rate, int numPayments)
+        {
+            double payment = Math.Round(principal * ((rate * Math.Pow((1 + rate), numPayments)) / (Math.Pow((1 + rate), numPayments) - 1)), 2);
+            return payment;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Amortization;
+
+Mortgage mortgage =  new Mortgage();
+
+Console.WriteLine("Enter your principal amount:");
+int principal = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Loan Term in Years:");
+int numPayments = int.Parse(Console.ReadLine()) * 12;
+
+Console.WriteLine("Enter your interest rate:");
+double rate = double.Parse(Console.ReadLine()) / 12 / 100;
+
+double payment = mortgage.MonthlyPayment(principal, rate, numPayments);
+Console.WriteLine($"Your monthly payment is: {payment}");
+
+Console.ReadLine();
